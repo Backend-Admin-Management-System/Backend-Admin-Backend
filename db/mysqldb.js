8f6387f4a6ce9b0782910d8d9a71f2898347bc9c
lily <lily.jiang@institutedata.com>
2024-03-18 19:23:42 +1000

    v1.0.1 - backend - init node.js mooc admin management system

---
+++ db/mysqldb.js
@@ -0,0 +1,15 @@
+// const mysql = require("mysql");//not support await/async
+
+const mysql2 = require('mysql2/promise');//support awat/async
+const { mysqlConfig } = require("../appConfig");
+
+const db = mysql2.createPool({
+  host: mysqlConfig.host,
+  user: mysqlConfig.user,
+  password: mysqlConfig.password,
+  database: mysqlConfig.database,
+});
+
+module.exports = {
+  db,
+};
